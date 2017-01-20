5719e7a9e9b496608f0b0c0c033f4e7d52c214a3
Ion Gorincioi <gorincioii@yahoo.com>
2017-01-20 10:25:28 -0800

    Create the main component, index.js and index.html

---
+++ app/components/Main.js
@@ -0,0 +1,50 @@
+let TextBox = React.createClass({
+  getInitialState: function() {
+    return {
+      text: "",
+      photoAdded: false
+    };
+  },
+  handleChange: function(event) {
+    this.setState({ text: event.target.value });
+  },
+  togglePhoto: function(event) {
+    this.setState({ photoAdded: !this.state.photoAdded });
+  },
+  remainingCharacters: function() {
+    if (this.state.photoAdded) {
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  },
+  countWords: function(){
+    return this.state.text.split(' ').length-1;
+  },
+  render: function() {
+    return (
+      <div className="well clearfix">
+        <textarea className="form-control"
+                  onChange={this.handleChange}></textarea>
+        <br/>
+        <span>{ this.remainingCharacters() }</span>
+        <br/>
+        <span>You have written "{ this.countWords() }" Words</span>
+        
+        <button className="btn btn-primary pull-right"
+          disabled={this.state.text.length === 0 && !this.state.photoAdded}><i className="fa fa-paper-plane"></i></button>
+        <button className="btn btn-default pull-right"
+          onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }
+        </button>
+      </div>
+    );
+  }
+});
+
+// ReactDOM.render(
+//   <TextBox />,
+//   document.getElementById("app")
+// );
+
+module.exports = TextBox;
---
+++ app/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TextBox = require('./Main');
+
+ReactDOM.render(TextBox, document.getElementById('app'));
